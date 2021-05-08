e662c93e0a79af0ab4afe78012fc37056edd56d7
Arsalan Khattak <me@khattak.dev>
2021-05-09 00:23:24 +0500

    invite people on opening issue

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
+    const invitee_id = context.payload.sender.id;
+    const labels = context.payload.issue.labels;
+    console.log("Send an Invitation to ", invitee_id);
+
+    for (const iterator of labels) {
+      if (iterator.name == "invitation request") {
+        return context.octokit.orgs.createInvitation({
+          org: "KhattakDevHub",
+          invitee_id,
+        });
+      }
+    }
+
+    // console.log(context.payload.organization.id);
+    // return context.octokit.orgs.createInvitation({
+
+    // })
+    // return context.octokit.issues.createComment(issueComment);
   });
 
   // For more information on building apps:
